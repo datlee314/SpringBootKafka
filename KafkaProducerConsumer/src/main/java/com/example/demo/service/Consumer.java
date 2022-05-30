9e32efc116971876f69ddb4406d132038e891a36
Delta <letuandat314@gmail.com>
2022-05-30 22:24:03 +0700

    commit2

---
+++ KafkaProducerConsumer/src/main/java/com/example/demo/service/Consumer.java
@@ -0,0 +1,12 @@
+package com.example.demo.service;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Consumer {
+	@KafkaListener(topics="mytopic", groupId="mygroup")
+	public void consumerFromtopic(String message) {
+		System.out.println("Consummed message "+message);
+	}
+}
